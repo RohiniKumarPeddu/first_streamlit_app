56ac4a19402ad3cdcd50890821376df17321e12e
Rohini Kumar Peddu <118859430+RohiniKumarPeddu@users.noreply.github.com>
2022-11-22 17:27:18 -0600

    Update streamlit_app.py
    Select fruit names instead of fruit index value

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.header("My Fruits List")
 
 # read fruit list from S3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # display fruit list
 streamlit.dataframe(my_fruit_list)
