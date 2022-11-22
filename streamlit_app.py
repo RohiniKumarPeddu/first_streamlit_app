5740dfbb96616806b8d8d6b2aefd70dc30fdf1d1
Rohini Kumar Peddu <118859430+RohiniKumarPeddu@users.noreply.github.com>
2022-11-22 17:08:45 -0600

    Update streamlit_app.py
    Added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title("My first streamlit App")
 
+streamlit.header("Required:")
+streamlit.text("Snowflake Account")
+streamlit.text("GMail Account")
+streamlit.text("GitHub Account")
+streamlit.text("Streamlit Account")
