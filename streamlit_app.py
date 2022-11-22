2c5396be44fe39d99d1cb403b6e88f749741df3f
Rohini Kumar Peddu <118859430+RohiniKumarPeddu@users.noreply.github.com>
2022-11-22 17:25:04 -0600

    Update streamlit_app.py
    Able to multi select fruits from the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,13 @@ streamlit.text("Streamlit Account")
 
 streamlit.header("My Fruits List")
 
+# read fruit list from S3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# display fruit list
 streamlit.dataframe(my_fruit_list)
+
+# create a pick list for user to pick one or more fruits
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+
+# display the list
