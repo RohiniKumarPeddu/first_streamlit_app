79c3cf13733a07dafe8696587fff592bdde09e16
Rohini Kumar Peddu <118859430+RohiniKumarPeddu@users.noreply.github.com>
2022-11-22 17:20:49 -0600

    Update streamlit_app.py
    Added fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My first streamlit App")
 
@@ -7,3 +8,8 @@ streamlit.text("❄️Snowflake Account")
 streamlit.text("📧GMail Account")
 streamlit.text("GitHub Account")
 streamlit.text("Streamlit Account")
+
+streamlit.header("My Fruits List")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
