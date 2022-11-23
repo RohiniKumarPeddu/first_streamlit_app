4cd1c77b27ac70878e1f9b88a1bfae5d358e6550
Rohini Kumar Peddu <118859430+RohiniKumarPeddu@users.noreply.github.com>
2022-11-23 12:23:02 -0600

    Update streamlit_app.py
    Added requests from fruityvice API response code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My first streamlit App")
 
@@ -24,3 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the selected list
 streamlit.dataframe(fruits_to_show)
+
+# display fruitvice api resonse
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
