c55a185038de84ccafc0db44392ef1b1c06242f4
Rohini Kumar Peddu <118859430+RohiniKumarPeddu@users.noreply.github.com>
2022-11-25 14:08:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,7 @@ def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
-
-#rows = run_query("select current_user(), current_account(), current_region();")
+    
 rows = run_query("select * from fruit_load_list;")
 
 st.header("Hello from Snowflake:")
